6198ac1591e2e8d09eadee80df2afb3322dff786
Dru Ibarra <Druwerd@gmail.com>
2015-01-07 16:51:31 -0800

    add spec for 5 by 5 multiplication table

--- spec/table_spec.rb
+++ spec/table_spec.rb
@@ -32,4 +32,30 @@ describe PrimeTime::Table do
     end
   end
 
+  context "5 by 5 multiplication table" do
+    subject(:five_by_five_table) { PrimeTime::Table.new(width: 5, height: 5)}
+    subject(:five_by_five_table_string) {
+%Q(x  1  2  3  4  5
+1  1  2  3  4  5
+2  2  4  6  8 10
+3  3  6  9 12 15
+4  4  8 12 16 20
+5  5 10 15 20 25)
+    }
+
+    before {
+      five_by_five_table.col_headers = %w(1  2  3  4  5)
+      five_by_five_table.row_headers = %w(1  2  3  4  5)
+      five_by_five_table.rows[0] = %w(1  2  3  4  5)
+      five_by_five_table.rows[1] = %w(2  4  6  8 10)
+      five_by_five_table.rows[2] = %w(3  6  9 12 15)
+      five_by_five_table.rows[3] = %w(4  8 12 16 20)
+      five_by_five_table.rows[4] = %w(5 10 15 20 25)
+    }
+
+    it "prints to STDOUT" do
+      expect(five_by_five_table.to_s).to eq(five_by_five_table_string)
+    end
+  end
+
 end
