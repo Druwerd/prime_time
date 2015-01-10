8d7b1aac1bad512adc33285b27dc40a2892032fd
Dru Ibarra <Druwerd@gmail.com>
2015-01-09 19:49:00 -0800

    implementation of ".is_prime?"

--- lib/prime_time/calculate.rb
+++ lib/prime_time/calculate.rb
@@ -6,8 +6,21 @@ module PrimeTime
 
       end
 
+      # worst case O(sqrt(n)/2)
       def is_prime?(n)
-        
+        n = n.to_i
+        return false if n == 1 || n < 0 # 1 and negative numbers are not prime
+        return true  if n == 2 # 2 is a special case the only even prime
+        return false if n % 2 == 0 # even numbers are not prime
+
+        # start with 3 and go up to the square root of n to see is divisable
+        divisor = 3
+        while divisor <= Math.sqrt(n)
+          return false if n % divisor == 0
+          divisor += 2
+        end
+
+        true
       end
     end
 
